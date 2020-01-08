9ba6e6fef78f6de84a5e6a963cdac1cd2af8f394
Damon Kelley <32469142+skelleyton@users.noreply.github.com>
2020-01-08 15:07:37 -0600

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>This Works!!!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  What's up yo?
   <!-- END -->
   
   </div>
